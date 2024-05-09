0d7cc6f850a99be20e922bec78bda7b426a725b6
LuizGuilhes123 <luizguilhermeinformat@gmail.com>
2024-05-09 13:53:17 -0300

    feat: mew project

--- provaGupy/src/main/java/controller/PessoaController.java
+++ provaGupy/src/main/java/controller/PessoaController.java
@@ -2,31 +2,37 @@ package controller;
 
 import entity.PessoaEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import service.PessoaService;
 
-@Controller
 @RestController
-@RequestMapping("/pessoas")
+@RequestMapping("/api/pessoas")
 public class PessoaController {
 
     @Autowired
     private PessoaService pessoaService;
 
     @PostMapping
-    public PessoaEntity criarPessoa(@RequestBody PessoaEntity pessoa) {
-        return pessoaService.criarPessoa(pessoa);
+    public ResponseEntity<PessoaEntity> criarPessoa(@RequestBody PessoaEntity pessoa) {
+        PessoaEntity novaPessoa = pessoaService.criarPessoa(pessoa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaPessoa);
     }
 
     @PutMapping("/{id}")
-    public PessoaEntity atualizarPessoa(@PathVariable Long id, @RequestBody PessoaEntity pessoa) {
-        return pessoaService.atualizarPessoa(id, pessoa);
+    public ResponseEntity<PessoaEntity> atualizarPessoa(@PathVariable Long id, @RequestBody PessoaEntity pessoa) {
+        PessoaEntity pessoaAtualizada = pessoaService.atualizarPessoa(id, pessoa);
+        return ResponseEntity.ok(pessoaAtualizada);
     }
 
     @GetMapping("/{id}")
-    public PessoaEntity consultarPessoa(@PathVariable Long id) {
-        return pessoaService.consultarPessoa(id);
+    public ResponseEntity<PessoaEntity> consultarPessoa(@PathVariable Long id) {
+        PessoaEntity pessoa = pessoaService.consultarPessoa(id);
+        if (pessoa != null) {
+            return ResponseEntity.ok(pessoa);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
-
 }
